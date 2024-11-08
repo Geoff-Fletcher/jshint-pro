be344bf1cee29c6cb5b2aec6ce73f840c0eb7b5b
Geoff Fletcher <xov4m05a@students.codeinstitute.net>
2024-11-08 17:43:53 +0000

    add proper , listing for selected options

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,27 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+
+    let optArray = [];
+
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
+
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
         method: "POST",
@@ -26,14 +45,14 @@ async function postForm(e) {
 
 }
 
-function displayErrors(data){
+function displayErrors(data) {
     let heading = `JSHint Results for ${data.file}`;
 
     if (data.total_errors === 0) {
         results = `<div class="no_errors">No errors reported!</div>`;
     } else {
         results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`
-        for (let error of data.error_list){
+        for (let error of data.error_list) {
             results += `<div>At line <span class="line">${error.line}</span>, `;
             results += `column <span class="column">${error.col}</span></div>`;
             results += `<div class="error">${error.error}</div>`;
