23c1c4651ecfa4f24dd47947e181b7cfdcbdf61e
Geoff Fletcher <xov4m05a@students.codeinstitute.net>
2024-11-08 17:34:59 +0000

    add report forms to asses js and return Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,11 +11,37 @@ async function postForm(e) {
     const response = await fetch(API_URL, {
         method: "POST",
         headers: {
-                    "Authorization": API_KEY,
-                 }
-                 body: form,
-        })
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
 
+    const data = await response.json()
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+}
+
+function displayErrors(data){
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`
+        for (let error of data.error_list){
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 async function getStatus(e) {
@@ -34,11 +60,11 @@ async function getStatus(e) {
 
 function displayStatus(data) {
 
-    let heading ="API Key Status";
+    let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
-   
-    document.getElementById("resultsModalTitle").innerText =  heading;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 
