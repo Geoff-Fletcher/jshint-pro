d78bf609cd5845fb1badafaa7d80db1449c69e92
Geoff Fletcher <xov4m05a@students.codeinstitute.net>
2024-11-08 15:20:00 +0000

    add API query and log function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-HFw9IKSln1Wd3d6w7lTrnr5tnI4
+const API_KEY = "HFw9IKSln1Wd3d6w7lTrnr5tnI"
+const API_URL = "https://ci-jshint.herokuapp.com/api"
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"))
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+}
